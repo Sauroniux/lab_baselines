e037f1b583759ed3df0674d6f9185e128cc23061
Augustinas Šimkus <augustinas.simkus000@gmail.com>
2022-11-10 11:59:59 +0200

    lab 5

--- lab5.asm
+++ lab5.asm
@@ -4,6 +4,14 @@
 ; Kvarcinio rezonatoriaus daznis 4 MHz
 ;********************************************************
 
+#define PUNKTAS 23
+
+#define PUNKTAS_15 PUNKTAS == 15
+#define PUNKTAS_18 PUNKTAS == 18
+#define PUNKTAS_20 PUNKTAS == 20
+#define PUNKTAS_22 PUNKTAS == 22
+#define PUNKTAS_23 PUNKTAS == 23
+
 ;********************************************************
 	LIST p=16F84 		;nustatomas MV tipas
 	#INCLUDE <p16F84a.inc> ;iskviecama rinkmena,
@@ -32,19 +40,68 @@ Kint2 EQU 0Eh ;registra 0Eh pavadinti Kint2
 		movwf TRISB 	;perkelti W registro turini i
 						;TRISB registra
 		bcf STATUS, 5 	;pereiti i 0 banka
-Start	movlw b'11100000' ;irasyti i W registra
-						;dvejetaini skaicu,
-						;pateikta tarp kabucu
-		movwf PORTB 	;perkelti W registro turini i
-						;PORTB registra
+		
+#if !PUNKTAS_23
+	Start	movlw b'11100000' ;irasyti i W registra
+							;dvejetaini skaicu,
+							;pateikta tarp kabucu
+			movwf PORTB 	;perkelti W registro turini i
+							;PORTB registra
 
-		call Velinimas 	;iskviesti paprograma Velinimas
+			call Velinimas 	;iskviesti paprograma Velinimas
 
-		comf PORTB, 1 	;invertuoti PORTB registro turini
+	#if PUNKTAS_15 != 0
+			swapf PORTB, 1
+	#else #if PUNKTAS_18 != 0
+			movlw b'00111111' ;įrašyti į W registrą
+							;dvejetainį skaičių,
+							;pateiktą tarp kabučių
+			andwf PORTB, 1 	;atlikti loginę IR operaciją tarp skaičių,
+							;esančių W registre ir PORTB registre
+	#else #if PUNKTAS_20
+			andlw b‘10000000‘ ;atlikti loginę IR operaciją
+							;tarp pateikto skaičiaus ir
+							;skaičiaus, esančio W registre
+			movwf PORTB 	;perkelti W registro turinį
+							;į PORTB registrą
+	#else #if PUNKTAS_22
+			xorlw b'10010101'
+			movwf PORTB
+	#else
+							;Pradine uzduotis
+			comf PORTB, 1 	;invertuoti PORTB registro turini
+	#endif
 
-		call Velinimas 	;iskviesti paprograma Velinimas
-		goto Start 		;pereiti i programos eiluta, pazymeta
-						;zyme Start
+			call Velinimas 	;iskviesti paprograma Velinimas
+			goto Start 		;pereiti i programos eiluta, pazymeta
+							;zyme Start
+#else
+	Start 	movlw b'00000001' ;įrašyti į W registrą
+							;dvejetainį skaičių,
+							;pateiktą tarp kabučių
+			movwf PORTB 	;perkelti W registro turinį į
+							;PORTB registrą
+			call Velinimas 	;iškviesti paprogramę
+							;Velinimas
+			rlf PORTB,1 	;atlikti skaičiaus, esančio
+							;registre PORTB,
+							;rotaciją į kairę
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			rlf PORTB,1
+			call Velinimas
+			goto Start 		;pereiti į programos eilutę,
+							;pažymėtą žyme Start 
+#endif
 
 ;*************************Paprograme********************
 Velinimas
