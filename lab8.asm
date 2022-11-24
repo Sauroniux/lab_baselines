c5b18ab38ca6e8544732c33c97b07d133e3ff7ab
Augustinas Šimkus <augustinas.simkus000@gmail.com>
2022-11-24 15:27:39 +0200

    8 done

--- lab8.asm
+++ lab8.asm
@@ -13,14 +13,58 @@
 						;konfiguracija MV
 ;******************************************************
 
-;**********************Kintamasis ***********************
-Kint1 EQU 0Ch ;registra 0Ch pavadinti Kint1
+;**********************Kintamieji ***********************
+Wreg EQU 0Ch ; ;registra 0Ch pavadinti Wreg
+Stat EQU 0Dh ;registra 0Dh pavadinti Stat
+Kint1 EQU 0Eh ;registra 0Eh pavadinti Kint1
+Kint2 EQU 0Fh ;registra 0Fh pavadinti Kint2
+Kint3 EQU 10h ;registra 10h pavadinti Kint3
 ;********************************************************
 
 		ORG 0x000 		;nurodomas pradinis programos adresas 
 
+		goto Pagrindineprogr
+;********** *Pertraukties programa laikui skaiciuoti*********
+
+		ORG 0x004 		;pertraukties programos
+						;pradinis adresas
+		movwf Wreg 		;išsaugoti W registro turini
+		movf STATUS,0 	;perkelti STATUS registro turini i W
+		movwf Stat 		;išsaugoti STATUS
+						;registro turini
+		bcf INTCON,2 	;išvalyti (ikelti „0“ i) pertraukties
+						;požymio bito T0IF skilti
+		decfsz Kint1,1 	;atimti vieneta iš kintamojo Kint1 ir,
+						;kai jis bus lygus nuliui,
+						;peršokti komanda goto Ten
+		goto Ten 		;pereiti i programos eilute,
+						;pažymeta žyme Ten
+		movlw d'2' 	;irašyti i W registra dešimtaini
+						;skaiciu, pateikta tarp kabuciu
+		movwf Kint1 	;perkelti W registro turini
+						;i Kint1 registra
+		decf PORTB,1 	;padidinti PORTB registro
+						;turini vienetu
+Ten
+		movlw d'12' 	;irašyti i W registra dešimtaini
+						;skaiciu, pateikta tarp kabuciu
+		movwf TMR0 		;perkelti W registro turini
+						;i TMR0 registra
+		movf Stat,0 	;perkelti Stat registro
+						;turini i W registra
+		movwf STATUS 	;perkelti W registro turini
+						;i STATUS (atkurti
+						;STATUS registro turini)
+		movf Wreg,0 	;atkurti W registro turini
+		retfie 			;grižti i pagrindine programa
+;*******************************************************
+Pagrindineprogr
+
 		clrf PORTA 		;išvalyti PORTA registra
 		clrf PORTB 		;išvalyti PORTB registra
+		
+		movlw b'11111111'
+		movwf PORTB 
 
 		bsf STATUS, 5 	;pereiti i 1 banka
 
@@ -38,121 +82,8 @@ Kint1 EQU 0Ch ;registra 0Ch pavadinti Kint1
 						;i TRISA registra
 						;(nustatyti prievado
 						;išvadus duomenims išvesti)
-		movlw b'00000111' ;irašyti i W registra dvejetaini
-						;skaiciu, pateikta tarp kabuciu
-		movwf OPTION_REG ;perkelti W registro turini i
-						;OPTION_REG registra
-		bcf STATUS, 5 	;pereiti i 0 banka
-
-		movlw d'16' 	;irašyti i W registra
-						;dešimtaini skaiciu,
-						;pateikta tarp kabuciu
-		movwf Kint1 	;perkelti W registro turini
-						;i Kint1 registra
-Start1 
-		movlw d'11' 	;irašyti i W registra
-						;dešimtaini skaiciu,
-						;pateikta tarp kabuciu
-		movwf TMR0 		;perkelti W registro turini
-						;i TMRO registra
-		
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		
-Start2
-		btfss INTCON,2 	;patikrinti INTCON
-						;registro 2 skilti,
-						;jei joje yra „1“, peršokti
-						;komanda goto Start2
-		goto Start2 	;pereiti i programos eilute, 
-						;pažymeta žyme Start2
-		bcf INTCON,2 	;ikelti „0“ i INTCON
-						;registro 2 skilti (išvalyti
-						;T0IF bita kitam ciklui)
-		decfsz Kint1,1 	;atimti vieneta iš
-						;kintamojo Kint1 ir,
-						;kai jis bus lygus nuliui,
-						;peršokti komanda
-						;goto Start1
-		goto Start1 	;pereiti i programos eilute,
-						;pažymeta žyme Start1
-			
-		movlw d'1'
-		addwf PORTB,1
-		
-		btfsc STATUS,0
-		incf PORTA
-			
-		;incf PORTB 		;padidinti PORTB registro
-						;turini vienetu
-		movlw d'16' 	;irašyti i W registra
-						;dešimtaini skaiciu,
-						;pateikta tarp kabuciu
-		movwf Kint1 	;perkelti W registro
-						;turini i Kint1 registra
-		goto Start1 	;pereiti i programos
-						;eilute, pažymeta
-						;žyme Start1
-		END ;programos pabaiga
-
-#if 0
-
-;17
-;**********************Kintamieji ***********************
-Wreg EQU 0Ch ; ;registra 0Ch pavadinti Wreg
-Stat EQU 0Dh ;registra 0Dh pavadinti Stat
-Kint1 EQU 0Eh ;registra 0Eh pavadinti Kint1
-Kint2 EQU 0Fh ;registra 0Fh pavadinti Kint2
-Kint3 EQU 10h ;registra 10h pavadinti Kint3
-;********************************************************
-
-;19
-		goto Pagrindineprogr
-;********** *Pertraukties programa laikui skaiciuoti*********
-
-		ORG 0x004 		;pertraukties programos
-						;pradinis adresas
-		movwf Wreg 		;išsaugoti W registro turini
-		movf STATUS,0 	;perkelti STATUS registro turini i W
-		movwf Stat 		;išsaugoti STATUS
-						;registro turini
-		bcf INTCON,2 	;išvalyti (ikelti „0“ i) pertraukties
-						;požymio bito T0IF skilti
-		decfsz Kint1,1 	;atimti vieneta iš kintamojo Kint1 ir,
-						;kai jis bus lygus nuliui,
-						;peršokti komanda goto Ten
-		goto Ten 		;pereiti i programos eilute,
-						;pažymeta žyme Ten
-		movlw d'16' 	;irašyti i W registra dešimtaini
-						;skaiciu, pateikta tarp kabuciu
-		movwf Kint1 	;perkelti W registro turini
-						;i Kint1 registra
-		incf PORTB,1 	;padidinti PORTB registro
-						;turini vienetu
-Ten
-		movlw d'12' 	;irašyti i W registra dešimtaini
-						;skaiciu, pateikta tarp kabuciu
-		movwf TMR0 		;perkelti W registro turini
-						;i TMR0 registra
-		movf Stat,0 	;perkelti Stat registro
-						;turini i W registra
-		movwf STATUS 	;perkelti W registro turini
-						;i STATUS (atkurti
-						;STATUS registro turini)
-		movf Wreg,0 	;atkurti W registro turini
-		retfie 			;grižti i pagrindine programa
-;*******************************************************
-Pagrindineprogr
-
-;20
+						
+; 20
 		bcf STATUS,5 	;pereiti i 0 banka
 		movlw d'12' 	;irašyti i W registra
 						;dešimtaini skaiciu,
@@ -179,7 +110,7 @@ Pagrindineprogr
 						;skilti (leisti pertraukti,
 						;persipildžius TMR0 registrui)
 		bsf INTCON, GIE ;leisti visas pertrauktis
-
+		
 ;21
 ;**************Žemo dažnio impulsu generatorius***********
 Start
@@ -218,5 +149,3 @@ Ciklas2
 		goto Start 		;pereiti i programos eilute,
 						;pažymeta žyme Start
 		END 			;programos pabaiga
-
-#endif
